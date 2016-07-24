a352c7c5c905fac127fe58f73854faf6b57547ac
tnoworyta <tomasz.noworyta@gmail.com>
2016-07-24 12:55:17 +0200

    Firebase, AngularFire2 installed, basic listing of items

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app
+  defaultFirebase({
+    apiKey: "AIzaSyBulVYHp3MWlg_Mx8OqmKmtQwu7Ao51ZZg",
+    authDomain: "mdoc1.firebaseapp.com",
+    databaseURL: "https://mdoc1.firebaseio.com",
+    storageBucket: "project-7103516944652309740.appspot.com"
+  })
+]);
