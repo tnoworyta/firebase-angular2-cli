5dfad4248185ce4371058bc36ab2abc183740dc5
tnoworyta <tomasz.noworyta@gmail.com>
2016-07-31 13:44:51 +0200

    Firebase auth enabled in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgForm }    from '@angular/forms';
 import { User }    from './../user';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -13,13 +14,18 @@ export class LoginComponent implements OnInit {
 
   model = new User('', '');
 
-  constructor() { }
+  constructor(public af: AngularFire) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
     console.log(this.model);
+    this.af.auth.login({ email: this.model.email, password: this.model.password });
     this.model = new User('', '');
+  }
+
+  logout() {
+    this.af.auth.logout()
   }
 }
